dc33798977c800e302d32b14c898c65d462c1313
MichaelDouglas17 <114314473+MichaelDouglas17@users.noreply.github.com>
2022-09-25 02:49:59 -0300

    Atualização final jogo.c

--- Jogo.c
+++ Jogo.c
@@ -1,22 +1,67 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 
 int main(){
 
     //Imprimindo cabecalho
 
-    printf("=========================================\n");
-    printf("   | Bem Vindo ao jogo de adivinhacao |\n");
-    printf("=========================================\n");
-    printf("\n");
+    printf("    .__________________________.                                  \n");
+    printf("    | .___________________. |==|                                  \n");
+    printf("    | | ................. | |  |                                  \n");
+    printf("    | | ::::Apple ][::::: | |  |                                  \n");
+    printf("    | | ::::::::::::::::: | |  |                                  \n");
+    printf("    | | ::::::::::::::::: | |  |            BEM VINDO AO GAME     \n");
+    printf("    | | ::::::::::::::::: | |  |                                  \n");
+    printf("    | | ::::::::::::::::: | |  |           AKINATOR DE NUMEROS    \n");
+    printf("    | | ::::::::::::::::: | | ,|              -  By MD  -         \n");
+    printf("    | !___________________! |(c|                                  \n");
+    printf("    !_______________________!__!                                  \n");
+    printf("   /                            \\              \n");
+    printf("  /  [][][][][][][][][][][][][]  \\             \n");
+    printf(" /  [][][][][][][][][][][][][][]  \\            \n");
+    printf("(  [][][][][____________][][][][]  )            \n");
+    printf(" \\ ------------------------------ /            \n");
+    printf("  \\______________________________/             \n");
+    printf("\n\n");
 
-    int numerosecreto = 42;
+    int segundos =  (0);
+    srand(segundos);
+
+    int numerogrande = rand();
+    int numerosecreto =  numerogrande % 100;
     int chute;
     int tentativa = 1;
+    double pontos = 1000;
+
+    int acertou = 0;
+
+    int nivel;
+    printf("Qual nivel de dificuldade?\n");
+    printf("(1) Facil (2) Medio (3)Dificil\n\n");
+    printf("Escolha: ");
+    scanf("%d", &nivel);
+
+    int numerodetentativas;
+
+    switch(nivel) {
+
+        case 1:
+            numerodetentativas = 20;
+            break;
+
+        case 2:
+            numerodetentativas = 15;
+            break;
+
+        default:
+            numerodetentativas = 6;
+            break;
+    }
 
 
-
-    while (1){
+    for (int i = 1; i <= numerodetentativas; i++)  {
 
         printf("(tentativa %d)\n", tentativa);
         printf("Qual e  o seu chute? ");
@@ -29,12 +74,11 @@ int main(){
             continue;
         }
 
-        int acertou = chute == numerosecreto;
+        acertou = chute == numerosecreto;
         int maior = chute > numerosecreto;
 
+
         if(acertou){
-            printf("Parabens! voce acertou!\n");
-            printf("Jogue novo\n.");
             break;
         }
 
@@ -49,8 +93,67 @@ int main(){
 
         printf("\n");
         tentativa++;
+
+
+        double pontosperdidos = abs(chute - numerosecreto ) / 2.0;
+        pontos = pontos - pontosperdidos;
     }
 
-    printf("==== FIM DE JOGO ====\n");
-    printf("Voce acertou em %d tentativas\n", tentativa);
+    if(acertou){
+
+
+            printf("                  *****************                                          \n");
+            printf("               ******               ******                                   \n");
+            printf("           ****                           ****                               \n");
+            printf("        ****                                 ***                             \n");
+            printf("      ***                                       ***                          \n");
+            printf("     **           ***               ***           **                         \n");
+            printf("   **           *******           *******          ***                       \n");
+            printf("  **            *******           *******            **                      \n");
+            printf(" **             *******           *******             **                     \n");
+            printf(" **               ***               ***               **                    PARABENS           \n");
+            printf("**                                                     **                    \n");
+            printf("**       *                                     *       **         Voce acertou em %d tentativas\n", tentativa);
+            printf("**      **                                     **      **                    \n");
+            printf(" **   ****                                     ****   **                     \n");
+            printf(" **      **                                   **      **                     \n");
+            printf("  **       ***                             ***       **             TOTAL DE PONTOS: %.2f\n", pontos);
+            printf("   ***       ****                       ****       ***                       \n");
+            printf("     **         ******             ******         **                         \n");
+            printf("      ***            ***************            ***                          \n");
+            printf("        ****                                 ****                            \n");
+            printf("           ****                           ****                               \n");
+            printf("               ******               ******                                   \n");
+            printf("                    *****************                                        \n");
+
+
+    }
+
+    else {
+
+
+
+        printf(" $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$'               `$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$                           \n");
+        printf(" $$$$$$$$$$$$$$$$$$$$$$$$$$$$'                   `$$$$$$$$$$$$$$$$$$$$$$$$$$$$                           \n");
+        printf(" $$$'`$$$$$$$$$$$$$'`$$$$$$!                       !$$$$$$'`$$$$$$$$$$$$$'`$$$                           \n");
+        printf(" $$$$  $$$$$$$$$$$  $$$$$$$                         $$$$$$$  $$$$$$$$$$$  $$$$          TROUXA           \n");
+        printf(" $$$$. `$' \\' \\$`  $$$$$$$!                         !$$$$$$$  '$/ `/ `$' .$$$$                         \n");
+        printf(" $$$$$. !\\  i  i .$$$$$$$$                           $$$$$$$$. i  i  /! .$$$$$      VOCE PERDEU!!!!     \n");
+        printf(" $$$$$$   `--`--.$$$$$$$$$                           $$$$$$$$$.--'--'   $$$$$$                           \n");
+        printf(" $$$$$$L        `$$$$$^^$$                           $$^^$$$$$'        J$$$$$$                           \n");
+        printf(" $$$$$$$.   .'   ""~   $$$    $.                 .$  $$$   ~""   `.   .$$$$$$$                           \n");
+        printf(" $$$$$$$$.  ;      .e$$$$$!    $$.             .$$  !$$$$$e,      ;  .$$$$$$$$                           \n");
+        printf(" $$$$$$$$$   `.$$$$$$$$$$$$     $$$.         .$$$   $$$$$$$$$$$$.'   $$$$$$$$$                           \n");
+        printf(" $$$$$$$$    .$$$$$$$$$$$$$!     $$`$$$$$$$$'$$    !$$$$$$$$$$$$$.    $$$$$$$$                           \n");
+        printf(" $JT&yd$     $$$$$$$$$$$$$$$$.    $    $$    $   .$$$$$$$$$$$$$$$$     $$$$$$$                           \n");
+        printf("                                  $    $$    $                                                           \n");
+        printf("                                  $.   $$   .$                                                           \n");
+        printf("                                  `$        $'                                                           \n");
+        printf("                                   `$$$$$$$$'                                                            \n");
+        printf("\n\n");
+    }
+
+
+    printf("\n==== FIM DE JOGO ====\n");
+
 }
